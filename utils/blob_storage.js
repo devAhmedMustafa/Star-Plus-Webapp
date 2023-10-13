a6680757ee3cf44c9993936b567be08be3a0e005
Ahmed Mustafa <61328457+devAhmedMustafa@users.noreply.github.com>
2023-10-13 07:24:57 +0300

    Some Fixes

--- utils/blob_storage.js
+++ utils/blob_storage.js
@@ -3,7 +3,7 @@ import { put } from "@vercel/blob";
 const blobUpload = async (file)=>{
     const { url } = await put(`articles/${file.name}`, file, { access: 'public', token: "vercel_blob_rw_HkgyrPpNbcUJOswO_nk711QQcAT244dnLdChHJnVEPvkMAg"});
 
-    return url;
+    return String(url);
 }
 
 export {blobUpload}
