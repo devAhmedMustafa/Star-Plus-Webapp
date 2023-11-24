597be18a93191f71d9388dacd0e30e1012003dde
Ahmed Mustafa <61328457+devAhmedMustafa@users.noreply.github.com>
2023-11-24 16:58:28 +0200

    Add New Members

--- constants/members.js
+++ constants/members.js
@@ -1,15 +1,23 @@
 const members = [
     { 
         name: 'Ahmed Mustafa',
-        desc: "A Software Engineer, Game Developer and Sound Designer."
+        desc: "Software Engineer, Game Developer and Sound Designer"
     },
     {
         name: 'Saif Abdelkareem',
-        desc: "A Game Designer and Graphic Animator",
+        desc: "Game Designer and Graphic Animator",
     },
     {
         name: 'Sohail Mohamed',
-        desc: "A Graphic Designer"
+        desc: "Graphic Designer"
+    },
+    {
+        name: 'Abdelrahman Elbana',
+        desc: "Game Developer"
+    },
+    {
+        name: 'Habiba Hossam',
+        desc: "Game Designer"
     }
 ]
 
