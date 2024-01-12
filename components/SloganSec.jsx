4f8fe9d12da35e8cd85c64c017269380523df527
Ahmed Mustafa <61328457+devAhmedMustafa@users.noreply.github.com>
2024-01-13 00:51:00 +0200

    Fixing Design

--- components/SloganSec.jsx
+++ components/SloganSec.jsx
@@ -5,7 +5,7 @@ export default function SloganSec({children}){
     
 
     return (
-       <div className="overflow-hidden md:h-[300px] relative">
+       <div className="overflow-hidden md:h-[300px] relative h-[250px]">
 
             {children}
 
